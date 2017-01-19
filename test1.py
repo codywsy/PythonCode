0fbcadf1ba9b53245485e3480a9c08ed7c7e7dbd
codywsy <codywsy@gmail.com>
2017-01-20 00:01:31 +0800

    testFile

--- test1.py
+++
@@ -1,4 +0,0 @@
-sum = 0;
-for x in list(range(4)):
-    sum = sum + x
-print(sum)
---
+++ testFile.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 打开一个文件
+foo = open("foo.txt", "w")
+print "文件名： ".decode("utf8"), foo.name
+print "是否已关闭：".decode("utf8"), foo.closed
+print "访问模式：".decode("utf8"), foo.mode
+print "末尾是否强制加空格：".decode("utf8"), foo.softspace
+
+# 写入新内容到文件
+foo.write("github.com/codywsy/**.git")
+
+# 关闭文件
+foo.close()
+
+foo = open("foo.txt", "r")
+str = foo.read(2)
+print "读取的字符串是：".decode("utf8"), str
+foo.close()
